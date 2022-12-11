b1c3d63beb074bab55314e78d3e56fea86c8833d
mariomamo <mariooffertucci@live.it>
2022-12-11 03:17:18 +0100

    password search + graphic improvements

--- app/src/components/AccountList/accountlist.js
+++ app/src/components/AccountList/accountlist.js
@@ -4,8 +4,8 @@ import './accountlist.css';
 
 const AccountList = ({onDelete, accountList})=> {
     return (
-        accountList !== undefined && accountList.map(account =>
-            <ListItem onDelete={onDelete} accountName={account}/>
+        accountList !== undefined && accountList.map((account, idx) =>
+            <ListItem key={idx} onDelete={onDelete} accountName={account}/>
         )
     )
 }
