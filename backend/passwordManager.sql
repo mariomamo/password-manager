2b4444d4ad8b7f41d111b183a671e8004ea542ab
mariomamo <mariooffertucci@live.it>
2023-03-28 00:30:10 +0200

    Added documentation

--- backend/passwordManager.sql
+++ backend/passwordManager.sql
@@ -2,7 +2,7 @@ CREATE DATABASE passwordManager;
 USE passwordManager;
 
 CREATE TABLE user(
-    username varchar(255) PRIMARY KEY NOT NULL,
+    username varchar(120) PRIMARY KEY NOT NULL,
     password varchar(255) NOT NULL,
     name varchar(255) NOT NULL,
     surename varchar(255) NOT NULL,
@@ -15,4 +15,16 @@ CREATE TABLE credential(
     secret varchar(1000) NOT NULL,
     PRIMARY KEY (username, name),
     FOREIGN KEY (username) REFERENCES user(username) ON UPDATE CASCADE ON DELETE CASCADE
-)
+);
+
+CREATE TABLE active_session(
+	session_id varchar(250) PRIMARY KEY NOT NULL,
+    username varchar(120),
+    FOREIGN KEY (username) REFERENCES user(username) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE session_variable(
+	session_id varchar(250) PRIMARY KEY NOT NULL,
+    value varchar(255) NOT NULL,
+    FOREIGN KEY (session_id) REFERENCES active_session(session_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
